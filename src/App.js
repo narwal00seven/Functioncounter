d8ea44f9901159f8094ce6c652ddaa2f7d760f0d
Saneep dalal <171ankit.2020@gmail.com>
2024-05-11 17:07:20 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { useState } from "react";
+export default function App() {
+  const [Count, setCount] = useState(0);
+  const increment = (e) => {
+    setCount(Count + 1);
+  };
+  const decrement = (e) => {
+    setCount(Count - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1>Counter App</h1>
+      <h3>Count: {Count}</h3>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
-
-export default App;
